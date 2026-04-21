6d096b8e367aadbf2c9e883fbc78b08a431a2a8c
ricard0g <ricardoguzman444@gmail.com>
2026-04-21 16:49:14 +0200

    feat: añade scrolleo smooth al clicar links y footer ya responsivo tambien.

--- web/landing/scripts/script.js
+++ web/landing/scripts/script.js
@@ -1,6 +1,42 @@
+// Mobile Navbar
 const mobileBtn = document.getElementById("mobile-button");
 const mobileNavList = document.querySelector(".header__inner-list-mobile");
 
 mobileBtn.addEventListener("click", (e) => {
 	mobileNavList.classList.toggle("show-inner-list-mobile");
 });
+
+mobileNavList.addEventListener("click", (e) => {
+	mobileNavList.classList.toggle("show-inner-list-mobile");
+});
+
+// Smooth scroll
+let navbar;
+if (window.innerWidth > 768) {
+	navbar = document.querySelector(".header__navbar");
+	console.log(navbar);
+} else {
+	navbar = document.querySelector(".header__navbar-mobile");
+	console.log(navbar);
+}
+
+navbar.addEventListener("click", (e) => {
+	const eTarget = e.target.closest("a");
+
+	e.preventDefault();
+
+	if (!eTarget || !e.target.getAttribute("href")?.startsWith("#")) return;
+
+	console.log(eTarget);
+	console.log(eTarget.getAttribute("href"));
+
+	const sectionSelected = document.getElementById(
+		eTarget.getAttribute("href").replace("#", ""),
+	);
+
+	sectionSelected.scrollIntoView({ behavior: "smooth" });
+});
+
+// Footer Date
+const yearDateSpan = document.getElementById("year-date");
+yearDateSpan.innerHTML = new Date().getFullYear();
